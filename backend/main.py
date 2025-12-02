7fc048b06da688aa4c1492e8d72966918a3a5ecb
Nabin <sharmanabin.0910@gmail.com>
2025-12-02 21:25:08 +0530

    Defined Basemodel

--- backend/main.py
+++ backend/main.py
@@ -9,3 +9,19 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv('OPEN AI KEY'))
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+class TranslationRequest(BaseModel);
+    text : str
